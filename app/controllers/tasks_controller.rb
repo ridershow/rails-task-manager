4b1631293f9ff031ce4aa71610886e5707f95b4e
jeremy lanfranchi <lanfranchi.jeremy@gmail.com>
2018-07-27 14:59:14 +0800

    We can add task to our task list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.new.find(user_params)
+    @task.update
+    redirect_to tasks_path
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  root to: 'tasks#index'
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get    "new",      to: "tasks#new"
+  post   "tasks",   to: "tasks#create"
 
 end
 
