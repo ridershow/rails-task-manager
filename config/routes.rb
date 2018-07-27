8f918e2d4a813fe253cedc037ab82078b204a217
jeremy lanfranchi <lanfranchi.jeremy@gmail.com>
2018-07-27 11:49:54 +0800

    created a db + model & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+
+
+# First, add a new route to list the tasks, following the convention from the lecture.
+# Then, add a controller action and its view.
+# This action should fetch all tasks, and a view should loop over these to display them.
